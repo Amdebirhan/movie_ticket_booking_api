006bb0613b58ba251c2c0bf374a3c18c399fad0a
Amdebirhan <amdebirhanteshome@gmail.com>
2022-07-15 09:10:30 -0700

    init

---
+++ config/config.js
@@ -0,0 +1,13 @@
+/*
+Configuration class to setup port and mongodb url .
+*/
+
+var env = process.env.NODE_ENV || 'development';
+
+if (env === 'development') {
+    process.env.PORT = 9090;
+    process.env.MONGODB_URI = 'mongodb://localhost:27017/MovieBookingApp';
+} else if (env === 'test') {
+    process.env.PORT = 9090;
+    process.env.MONGODB_URI = 'mongodb://localhost:27017/MovieBookingAppTest';
+}
